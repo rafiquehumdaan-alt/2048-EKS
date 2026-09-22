264f65ce598ef7ec7ba85c82bf91dd095bd3bce0
Humdaan <rafiquehumdaan@gmail.com>
2026-09-22 19:51:58 +0100

    create the file tree for tf and finish oidc as well as start of ecr tf, test remote backend works

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -1,3 +1,8 @@
 resource "aws_ecr_repository" "ecr_repository" {
-  name = var.repository_name
+  name                 = var.repository_name
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 }
--- terraform/modules/ecr/outputs.tf
+++ terraform/modules/ecr/outputs.tf
@@ -2,3 +2,8 @@ output "repository_url" {
   description = "URL of the ECR repository"
   value       = aws_ecr_repository.ecr_repository.repository_url
 }
+
+output "repository_arn" {
+  description = "ARN of the ECR repository"
+  value       = aws_ecr_repository.ecr_repository.arn
+}
